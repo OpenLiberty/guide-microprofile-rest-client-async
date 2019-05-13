1636a7e346ece7ecc82ea5061750bc1fab9f95be
Panagiotis Roubatsis <panagiotis.roubatsis@ibm.com>
2019-05-13 10:07:06 -0400

    First commit

---
+++ finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -0,0 +1,35 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.inventory.client;
+
+import java.util.Properties;
+import java.util.concurrent.CompletionStage;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient
+@RegisterProvider(UnknownUrlExceptionMapper.class)
+@Path("/system/properties")
+public interface SystemClient {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletionStage<Properties> getProperties();
+
+}
