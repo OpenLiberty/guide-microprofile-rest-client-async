e55f10996eb5a43a8589c1c9240f67394aaa292f
Austin Seto <austin.seto@ibm.com>
2020-06-05 16:01:39 -0400

    Added javax.ws.rs imports to InventoryClient

--- finish/gateway/src/main/java/io/openliberty/guides/gateway/client/InventoryClient.java
+++ finish/gateway/src/main/java/io/openliberty/guides/gateway/client/InventoryClient.java
@@ -15,6 +15,13 @@ package io.openliberty.guides.gateway.client;
 import org.eclipse.microprofile.faulttolerance.Asynchronous;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.concurrent.CompletionStage;
