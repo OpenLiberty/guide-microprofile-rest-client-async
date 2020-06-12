fff94ec7f7f8c84e72281aeff57a5abe13656a5c
Austin Seto <austin.seto@ibm.com>
2020-06-12 09:51:44 -0400

    Script update

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -7,38 +7,31 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-# TEST 1:  Running the application in a Docker container
-mvn -q clean package
+./scripts/pacakageApps.sh
 
-docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+mvn -pl system verify
+mvn -pl inventory verify
+mvn -pl gateway verify
 
-docker build -t system:1.0-SNAPSHOT system/.
-docker build -t inventory:1.0-SNAPSHOT inventory/.
-docker build -t gateway:1.0-SNAPSHOT gateway/.
+./scripts/buildImages.sh
+./scripts/startContainers.sh
 
-docker run -d --name gettingstarted-app -p 9080:9080 openliberty-getting-started:1.0-SNAPSHOT
+sleep 180
 
-sleep 60
+docker logs system
+docker logs inventory
+docker logs gateway
 
-status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/system/properties")" 
-if [ "$status" == "200" ]
-then 
-  echo ENDPOINT OK
-else 
-  echo "$status" 
-  echo ENDPOINT NOT OK
+systemCPULoad="$(curl --write-out "%{http_code}" --silent --output /dev/null "http://localhost:9080/api/gateway/systems")"
+
+if [ "$systemCPULoad" == "200" ]
+then
+  echo SystemInventory OK
+else
+  echo System Inventory status:
+  echo "$systemCPULoad"
+  echo ENDPOINT
   exit 1
 fi
 
-docker stop gettingstarted-app && docker rm gettingstarted-app
-
-# TEST 2: Building and running the application
-mvn -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn liberty:start
-mvn failsafe:integration-test liberty:stop
-mvn failsafe:verify
-mvn liberty:package -Dinclude=runnable
-if [ ! -f "target/guide-getting-started.jar" ]; then
-    echo "target/guide-getting-started.jar was not generated!"
-    exit 1
-fi
+./scripts/stopContainers.sh
