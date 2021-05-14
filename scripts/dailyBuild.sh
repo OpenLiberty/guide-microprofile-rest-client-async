163583a2f66ebfaf9c02997b10df0de89e2da496
Priyanshi Patel <priyanshi.patel@ibm.com>
2021-05-14 16:46:33 -0400

    Update scripts

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -12,7 +12,7 @@ done
 
 echo "Testing daily OpenLiberty image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml query/pom.xml inventory/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install></configuration>" system/pom.xml query/pom.xml inventory/pom.xml
 cat system/pom.xml query/pom.xml inventory/pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:full-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile query/Dockerfile inventory/Dockerfile
