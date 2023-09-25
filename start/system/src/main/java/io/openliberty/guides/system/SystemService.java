74e98eacae9008a5ae2728ce63a0c0f61587aca7
Ruilin Ma <Ruilin.Ma@ibm.com>
2023-09-25 17:08:45 -0400

    mp6-ee10-update drafted

--- start/system/src/main/java/io/openliberty/guides/system/SystemService.java
+++ start/system/src/main/java/io/openliberty/guides/system/SystemService.java
@@ -2,12 +2,11 @@
 /*******************************************************************************
  * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Contributors:
- *     IBM Corporation - Initial implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 // end::copyright[]
 package io.openliberty.guides.system;
@@ -18,7 +17,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.reactivestreams.Publisher;
@@ -29,7 +28,7 @@ import io.reactivex.rxjava3.core.Flowable;
 @ApplicationScoped
 public class SystemService {
 
-    private static final OperatingSystemMXBean osMean = 
+    private static final OperatingSystemMXBean OS_MEAN = 
             ManagementFactory.getOperatingSystemMXBean();
     private static String hostname = null;
 
@@ -48,7 +47,7 @@ public class SystemService {
     public Publisher<SystemLoad> sendSystemLoad() {
         return Flowable.interval(15, TimeUnit.SECONDS)
                 .map((interval -> new SystemLoad(getHostname(),
-                        osMean.getSystemLoadAverage())));
+                    OS_MEAN.getSystemLoadAverage())));
     }
 
 }
