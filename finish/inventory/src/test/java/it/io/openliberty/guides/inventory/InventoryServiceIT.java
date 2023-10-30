1540f71a5f9697959c388e999b7dc5826be268e5
Ruilin Ma <Ruilin.Ma@ibm.com>
2023-10-30 16:53:03 -0400

    Update integration tests

--- finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryServiceIT.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2020, 2021 IBM Corporation and others.
+ * Copyright (c) 2020, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -11,42 +11,118 @@
 // end::copyright[]
 package it.io.openliberty.guides.inventory;
 
+import java.util.List;
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.nio.file.Paths;
 import java.util.Properties;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.microshed.testing.SharedContainerConfig;
-import org.microshed.testing.jaxrs.RESTClient;
-import org.microshed.testing.jupiter.MicroShedTest;
-import org.microshed.testing.kafka.KafkaProducerClient;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.client.ClientBuilder;
 
-import io.openliberty.guides.inventory.InventoryResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.Network;
+import org.testcontainers.utility.DockerImageName;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
 import io.openliberty.guides.models.SystemLoad;
 import io.openliberty.guides.models.SystemLoad.SystemLoadSerializer;
 
-@MicroShedTest
-@SharedContainerConfig(AppContainerConfig.class)
-@TestMethodOrder(OrderAnnotation.class)
+@Testcontainers
 public class InventoryServiceIT {
 
-    @RESTClient
-    public static InventoryResource inventoryResource;
+    private static Logger logger = LoggerFactory.getLogger(InventoryServiceIT.class);
 
-    @KafkaProducerClient(valueSerializer = SystemLoadSerializer.class)
+    public static InventoryResourceCleint client;
+
+    private static Network network = Network.newNetwork();
+
     public static KafkaProducer<String, SystemLoad> producer;
 
+    private static ImageFromDockerfile inventoryImage
+        = new ImageFromDockerfile("inventory:1.0-SNAPSHOT")
+            .withDockerfile(Paths.get("./Dockerfile"));
+
+    private static KafkaContainer kafkaContainer = new KafkaContainer(
+        DockerImageName.parse("confluentinc/cp-kafka:latest"))
+            .withListener(() -> "kafka:19092")
+            .withNetwork(network);
+
+    private static GenericContainer<?> inventoryContainer =
+        new GenericContainer(inventoryImage)
+            .withNetwork(network)
+            .withExposedPorts(9085)
+            .waitingFor(Wait.forHttp("/health/ready").forPort(9085))
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .withLogConsumer(new Slf4jLogConsumer(logger))
+            .dependsOn(kafkaContainer);
+
+    private static InventoryResourceCleint createRestClient(String urlPath) {
+        ClientBuilder builder = ResteasyClientBuilder.newBuilder();
+        ResteasyClient client = (ResteasyClient) builder.build();
+        ResteasyWebTarget target = client.target(UriBuilder.fromPath(urlPath));
+        return target.proxy(InventoryResourceCleint.class);
+    }
+
+    @BeforeAll
+    public static void startContainers() {
+        kafkaContainer.start();
+        inventoryContainer.withEnv(
+            "mp.messaging.connector.liberty-kafka.bootstrap.servers", "kafka:19092");
+        inventoryContainer.start();
+        client = createRestClient("http://"
+            + inventoryContainer.getHost()
+            + ":" + inventoryContainer.getFirstMappedPort());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Properties producerProps = new Properties();
+        producerProps.put(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaContainer.getBootstrapServers());
+        producerProps.put(
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        producerProps.put(
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                SystemLoadSerializer.class.getName());
+
+        producer = new KafkaProducer<String, SystemLoad>(producerProps);
+    }
+
     @AfterAll
-    public static void cleanup() {
-        inventoryResource.resetSystems();
+    public static void stopContainers() {
+        inventoryContainer.stop();
+        kafkaContainer.stop();
+        network.close();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        producer.close();
     }
 
     @Test
@@ -54,11 +130,11 @@ public class InventoryServiceIT {
         SystemLoad sl = new SystemLoad("localhost", 1.1);
         producer.send(new ProducerRecord<String, SystemLoad>("system.load", sl));
         Thread.sleep(5000);
-        List<String> response = inventoryResource.getSystems();
+        List<String> response = client.getSystems();
         assertNotNull(response);
         assertEquals(response.size(), 1);
         for (String system : response) {
-            Properties sp = inventoryResource.getSystem(system);
+            Properties sp = client.getSystem(system);
             assertEquals(sl.hostname, sp.get("hostname"),
                     "Hostname doesn't match!");
             BigDecimal systemLoad = (BigDecimal) sp.get("systemLoad");
--- finish/query/src/test/java/it/io/openliberty/guides/query/QueryServiceIT.java
+++ finish/query/src/test/java/it/io/openliberty/guides/query/QueryServiceIT.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -11,33 +11,53 @@
 // end::copyright[]
 package it.io.openliberty.guides.query;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Properties;
-import org.junit.jupiter.api.BeforeAll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.Test;
-import org.microshed.testing.jaxrs.RESTClient;
-import org.microshed.testing.jupiter.MicroShedTest;
-import org.microshed.testing.SharedContainerConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockserver.client.MockServerClient;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 
-import io.openliberty.guides.query.QueryResource;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.utility.DockerImageName;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@MicroShedTest
-@SharedContainerConfig(AppContainerConfig.class)
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.UriBuilder;
+
 public class QueryServiceIT {
 
-    @RESTClient
-    public static QueryResource queryResource;
+    private static Logger logger = LoggerFactory.getLogger(QueryServiceIT.class);
 
-    private static String testHost1 = 
+    public static QueryResourceClient client;
+
+    private static Network network = Network.newNetwork();
+    
+    public static String restClientClass = "InventoryClient.class";
+
+    private static String testHost1 =
         "{" + 
             "\"hostname\" : \"testHost1\"," +
             "\"systemLoad\" : 1.23" +
         "}";
-    private static String testHost2 = 
+    private static String testHost2 =
         "{" + 
             "\"hostname\" : \"testHost2\"," +
             "\"systemLoad\" : 3.21" +
@@ -48,47 +68,106 @@ public class QueryServiceIT {
             "\"systemLoad\" : 2.13" +
         "}";
 
-    @BeforeAll
-    public static void setup() throws InterruptedException {
-        AppContainerConfig.mockClient.when(HttpRequest.request()
-                                         .withMethod("GET")
-                                         .withPath("/inventory/systems"))
-                                     .respond(HttpResponse.response()
-                                         .withStatusCode(200)
-                                         .withBody("[\"testHost1\"," + 
-                                                    "\"testHost2\"," +
-                                                    "\"testHost3\"]")
-                                         .withHeader("Content-Type", "application/json"));
+    private static ImageFromDockerfile queryImage
+        = new ImageFromDockerfile("query:1.0-SNAPSHOT")
+              .withDockerfile(Paths.get("./Dockerfile"));
 
-        AppContainerConfig.mockClient.when(HttpRequest.request()
-                                         .withMethod("GET")
-                                         .withPath("/inventory/systems/testHost1"))
-                                     .respond(HttpResponse.response()
-                                         .withStatusCode(200)
-                                         .withBody(testHost1)
-                                         .withHeader("Content-Type", "application/json"));
+    public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
+        .parse("mockserver/mockserver")
+        .withTag("mockserver-" + MockServerClient.class
+                .getPackage().getImplementationVersion());
 
-        AppContainerConfig.mockClient.when(HttpRequest.request()
-                                         .withMethod("GET")
-                                         .withPath("/inventory/systems/testHost2"))
-                                     .respond(HttpResponse.response()
-                                         .withStatusCode(200)
-                                         .withBody(testHost2)
-                                         .withHeader("Content-Type", "application/json"));
+    public static MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)
+                    .withNetworkAliases("mock-server")
+                    .withNetwork(network);
+    public static MockServerClient mockClient;
 
-        AppContainerConfig.mockClient.when(HttpRequest.request()
-                                         .withMethod("GET")
-                                         .withPath("/inventory/systems/testHost3"))
-                                     .respond(HttpResponse.response()
-                                         .withStatusCode(200)
-                                         .withBody(testHost3)
-                                         .withHeader("Content-Type", "application/json"));
+    private static KafkaContainer kafkaContainer = new KafkaContainer(
+        DockerImageName.parse("confluentinc/cp-kafka:latest"))
+            .withListener(() -> "kafka:19092")
+            .withNetwork(network);
+
+    private static GenericContainer<?> queryContainer =
+        new GenericContainer(queryImage)
+            .withNetwork(network)
+            .withExposedPorts(9080)
+            .waitingFor(Wait.forHttp("/health/ready"))
+            .withStartupTimeout(Duration.ofMinutes(3))
+            .withLogConsumer(new Slf4jLogConsumer(logger))
+            .dependsOn(kafkaContainer);
+
+    private static QueryResourceClient createRestClient(String urlPath) {
+        ClientBuilder builder = ResteasyClientBuilder.newBuilder();
+        ResteasyClient client = (ResteasyClient) builder.build();
+        ResteasyWebTarget target = client.target(UriBuilder.fromPath(urlPath));
+        return target.proxy(QueryResourceClient.class);
     }
 
-    // tag::testLoads[]
+    @BeforeAll
+    public static void startContainers() {
+        mockServer.start();
+        mockClient = new MockServerClient(
+            mockServer.getHost(), 
+            mockServer.getServerPort());
+
+        kafkaContainer.start();
+
+        queryContainer.withEnv(
+            "InventoryClient/mp-rest/uri", "http://mock-server:" + MockServerContainer.PORT);
+        queryContainer.start();
+
+        client = createRestClient("http://"
+            + queryContainer.getHost()
+            + ":" + queryContainer.getFirstMappedPort());
+    }
+    @BeforeEach
+    public void setup() throws InterruptedException {
+        mockClient.when(HttpRequest.request()
+                        .withMethod("GET")
+                        .withPath("/inventory/systems"))
+                    .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withBody("[\"testHost1\"," + 
+                                "\"testHost2\"," +
+                                "\"testHost3\"]")
+                        .withHeader("Content-Type", "application/json"));
+
+        mockClient.when(HttpRequest.request()
+                        .withMethod("GET")
+                        .withPath("/inventory/systems/testHost1"))
+                    .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withBody(testHost1)
+                        .withHeader("Content-Type", "application/json"));
+
+        mockClient.when(HttpRequest.request()
+                        .withMethod("GET")
+                        .withPath("/inventory/systems/testHost2"))
+                    .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withBody(testHost2)
+                        .withHeader("Content-Type", "application/json"));
+
+        mockClient.when(HttpRequest.request()
+                        .withMethod("GET")
+                        .withPath("/inventory/systems/testHost3"))
+                    .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withBody(testHost3)
+                        .withHeader("Content-Type", "application/json"));
+    }
+
+    @AfterAll
+    public static void stopContainers() {
+        queryContainer.stop();
+        kafkaContainer.stop();
+        mockServer.stop();
+        network.close();
+    }
+
     @Test
-    public void testLoads() {
-        Map<String, Properties> response = queryResource.systemLoad();
+    public void testLoads(){
+        Map<String, Properties> response = client.systemLoad();
 
         assertEquals(
             "testHost2",
@@ -101,6 +180,4 @@ public class QueryServiceIT {
             "Returned lowest system load incorrect"
         );
     }
-    // end::testLoads[]
-
 }
