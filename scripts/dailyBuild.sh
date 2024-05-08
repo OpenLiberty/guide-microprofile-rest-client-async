673477935deee1708e42f5f9357e3bc53f3261b2
Gilbert Kwan <gkwan@ca.ibm.com>
2024-05-08 09:20:20 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -16,10 +16,10 @@ sed -i "\#</containerRunOpts>#a<install><runtimeUrl>https://public.dhe.ibm.com/i
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install></configuration>" query/pom.xml
 cat system/pom.xml query/pom.xml inventory/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile query/Dockerfile inventory/Dockerfile
-sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile query/Dockerfile inventory/Dockerfile
-cat system/Dockerfile query/Dockerfile inventory/Dockerfile
-
-docker pull -q "$DOCKER_USERNAME""/olguides:""$BUILD"
+if [[ "$DOCKER_USERNAME" != "" ]]; then
+    sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile query/Dockerfile inventory/Dockerfile
+    sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile query/Dockerfile inventory/Dockerfile
+    cat system/Dockerfile query/Dockerfile inventory/Dockerfile
+fi
 
 sudo ../scripts/testApp.sh
